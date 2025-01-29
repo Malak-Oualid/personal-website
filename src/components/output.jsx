908ae1ce37904f48c47400be9b89859d5a6c1df6
Malak Oualid <malak.oualid@mail.mcgill.ca>
2025-01-29 18:40:16 -0500

    added routing to experiences

--- src/components/output.jsx
+++ src/components/output.jsx
@@ -1,20 +1,32 @@
-import React from 'react';
-import Card from './card';
+import React from "react";
+import Card from "./card";
+import { NavLink } from "react-router-dom";
 
 function Outputs() {
-    return (
-        <div className="grid grid-cols-3 gap-4 p-4">
-        <Card title="Card Title"
-        description="This is a description of the card."
-        image="https://i.pinimg.com/474x/e5/9a/77/e59a77db5ce13395ffe21da2709f6a98.jpg"/>
-        <Card title="Card Title"
-        description="This is a description of the card."
-        image="https://i.pinimg.com/474x/e5/9a/77/e59a77db5ce13395ffe21da2709f6a98.jpg"/>
-        <Card title="Card Title"
-        description="This is a description of the card."
-        image="https://i.pinimg.com/474x/e5/9a/77/e59a77db5ce13395ffe21da2709f6a98.jpg"/>
-        </div>
-    
-    );
+  return (
+    <div className="grid grid-cols-3 gap-4 p-4">
+      <NavLink to="/work">
+        <Card
+          title="WORK"
+          description="A summary of my professional experience."
+          image="https://i.pinimg.com/474x/e5/9a/77/e59a77db5ce13395ffe21da2709f6a98.jpg"
+        />
+      </NavLink>
+      <NavLink to="/projects">
+        <Card
+          title="PROJECTS"
+          description="Some projects I have done ever since my first Hello World."
+          image="https://i.pinimg.com/474x/e5/9a/77/e59a77db5ce13395ffe21da2709f6a98.jpg"
+        />
+      </NavLink>
+      <NavLink to="/quests">
+        <Card
+          title="QUESTS"
+          description="A collection of my personal hobbies and silly shenanigans I persue on my free time."
+          image="https://i.pinimg.com/474x/e5/9a/77/e59a77db5ce13395ffe21da2709f6a98.jpg"
+        />
+      </NavLink>
+    </div>
+  );
 }
 export default Outputs;
