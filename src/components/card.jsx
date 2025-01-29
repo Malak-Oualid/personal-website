d72378fda300476cbfc7725e0d867ea4a38e5b5c
Malak Oualid <malak.oualid@mail.mcgill.ca>
2025-01-29 17:50:32 -0500

    desired card styles

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
 function Card({ title, description, image }) {
-    return (
-        <div className="flex flex-col items-center justify-center p-4 space-y-2">
-        <img src={image} alt={title} className="w-40 h-40 rounded-full" />
-        <h2 className="text-xl font-semibold">{title}</h2>
-        <p className="text-sm text-center">{description}</p>
-        </div>
-    );
-    }
+  return (
+    <div className="relative flex flex-col items-center justify-center p-4 space-y-2">
+      <div className="relative">
+        <img src={image} alt={title} className="w-100 h-100 rounded-lg" />
+        <h2 className="absolute inset-0 flex items-center justify-center text-2xl font-semibold text-white text-center p-2 rounded-b-lg">
+          {title}
+        </h2>
+      </div>
+      <p className="text-sm text-center">{description}</p>
+    </div>
+  );
+}
+
 export default Card;
--- src/components/output.jsx
+++ src/components/output.jsx
@@ -3,9 +3,18 @@ import Card from './card';
 
 function Outputs() {
     return (
+        <div className="grid grid-cols-3 gap-4 p-4">
         <Card title="Card Title"
         description="This is a description of the card."
         image="https://i.pinimg.com/474x/e5/9a/77/e59a77db5ce13395ffe21da2709f6a98.jpg"/>
+        <Card title="Card Title"
+        description="This is a description of the card."
+        image="https://i.pinimg.com/474x/e5/9a/77/e59a77db5ce13395ffe21da2709f6a98.jpg"/>
+        <Card title="Card Title"
+        description="This is a description of the card."
+        image="https://i.pinimg.com/474x/e5/9a/77/e59a77db5ce13395ffe21da2709f6a98.jpg"/>
+        </div>
+    
     );
 }
 export default Outputs;
